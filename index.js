b249271a39446be98400c1aecaaebe33e6860da6
Sanath Kumar Ramesh <sanathkr@amazon.com>
2017-12-01 08:57:00 -0800

    Tracing & SQS

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 'use strict';
 var fs = require('fs');
+var AWSXRay = require('aws-xray-sdk');
+var AWS = AWSXRay.captureAWS(require('aws-sdk'));
+
+var sqs = new AWS.SQS();
 
  exports.get = function(event, context) {
    var contents = fs.readFileSync("public/index.html");
-   context.succeed({
-     statusCode: 200,
-     body: contents.toString(),
-     headers: {'Content-Type': 'text/html'}
-   });
+
+   sqs.sendMessage({
+     MessageBody: "hello world",
+     QueueUrl: "https://sqs.us-east-1.amazonaws.com/283125242610/reinventdemo-queue"
+   }, function (err, data) {
+      var statusCode = 200;
+      if (err) throw err;
+
+      context.succeed({
+        statusCode: statusCode,
+        body: contents.toString(),
+        headers: {'Content-Type': 'text/html'}
+      });
+
+   })
+
+
  };
+
+
+
